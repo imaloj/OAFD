59dce23ad2ce9d57634e7699b25ae3e45734097d
Ripper <ripperk821@gmail.com>
2024-01-18 22:35:19 +0545

    first commit

---
+++ registration/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+def HomePage(request):
+    return render(request,'home.html')
+def SignupPage(request):
+    if request.method=='POST':
+        uname=request.POST.get('username'),
+        email=request.POST.get('email'),
+        pass1=request.POST.get('password1'),
+        pass2=request.POST.get('password2')
+
+        print(uname,email,pass1,pass2)
+    return render(request,'signup.html')
+def LoginPage(request):
+    return render(request,'login.html')
