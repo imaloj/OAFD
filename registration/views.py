8f59cfcf4d51bbc32854249784d459422f914c21
Ripper <ripperk821@gmail.com>
2024-01-19 18:19:17 +0545

    comment

--- registration/views.py
+++ registration/views.py
@@ -1,15 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.models import User
 
 def HomePage(request):
     return render(request,'home.html')
 def SignupPage(request):
+   #return HttpResponse("Username already exists. Please choose a different one.")
     if request.method=='POST':
-        uname=request.POST.get('username'),
-        email=request.POST.get('email'),
-        pass1=request.POST.get('password1'),
+        uname=request.POST.get('username')
+        email=request.POST.get('email')
+        pass1=request.POST.get('password1')
         pass2=request.POST.get('password2')
-
-        print(uname,email,pass1,pass2)
+        if pass1!=pass2:
+            return HttpResponse("Your passwords doesn't match")
+        else :
+            my_user=User.objects.create_user(uname,email,pass1)
+        my_user.save()
+        return redirect('login')
+        
     return render(request,'signup.html')
 def LoginPage(request):
     return render(request,'login.html')
+ 
