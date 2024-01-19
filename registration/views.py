2bf151493251a85217126b85ceefa07fb5fb889c
Ripper <ripperk821@gmail.com>
2024-01-19 19:46:37 +0545

    comment

--- registration/views.py
+++ registration/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,HttpResponse,redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login
 
 def HomePage(request):
     return render(request,'home.html')
@@ -19,5 +20,15 @@ def SignupPage(request):
         
     return render(request,'signup.html')
 def LoginPage(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        pass1=request.POST.get('password')
+        user=authenticate(request,username=username,password=pass1)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            return HttpResponse("Username or Password is incorrect!!!")
     return render(request,'login.html')
+    
  
