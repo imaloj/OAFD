839e6a5c083a7a8ef64ca7f690edddc94101192c
Ripper <ripperk821@gmail.com>
2024-01-27 22:20:02 +0545

    models

--- registration/models.py
+++ registration/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+class Student(models.Model):
+ STU_ID=models.CharField(max_length=20,unique=True,null=False, primary_key=True),
+ STU_NAME=models.CharField(max_length=20,null=False),
+ STU_FACULTY=models.CharField( max_length=50,null=False)
+ STU_ADD=models.CharField(max_length=50,null=False),
+ STU_PHONE=models.CharField(max_length=10,null=False),
+ STU_EMAIL=models.CharField(max_length=50,null=False),
+ Photo_taken= models.BooleanField 
+ def __str__(self):
+    return self.name 
 
-# Create your models here.
+class Teacher(models.Model):
+    STU_ID=models.CharField(max_length=20,unique=True,null=False, primary_key=True),    
