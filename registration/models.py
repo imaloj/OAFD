ae73b9c10e66922c645457353cd7068f8d3c95e2
Ripper <ripperk821@gmail.com>
2024-01-28 21:43:26 +0545

    created teacher user

--- registration/models.py
+++ registration/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+gender_list=(('Male','Male'),('Female','Female'))
+class Teacher(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE),
+    gender =models.CharField(max_length=10,choices=gender_list),
+    room=models.CharField(max_length=5),
+    is_delete =models.BooleanField(default=False)
 class Student(models.Model):
  STU_ID=models.CharField(max_length=20,unique=True,null=False, primary_key=True),
  STU_NAME=models.CharField(max_length=20,null=False),
@@ -10,5 +17,5 @@ class Student(models.Model):
  def __str__(self):
     return self.name 
 
-class Teacher(models.Model):
-    STU_ID=models.CharField(max_length=20,unique=True,null=False, primary_key=True),    
+# class Teacher(models.Model):
+#     STU_ID=models.CharField(max_length=20,unique=True,null=False, primary_key=True),    
